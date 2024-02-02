5e6f7769f1bbb7886e2ade538622311aded58f61
Jefferson <jefferson.brandao@g.globo>
2024-02-01 21:05:04 -0300

    config clerk and convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button>click</Button>
+    <div className="flex flex-col gap-y-4">
+      apenas authenticados
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://major-caribou-46.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
