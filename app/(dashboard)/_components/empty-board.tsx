fa9b0c5b0f994b824f9cf71837883e46db076f4c
Jefferson <jefferson.brandao@g.globo>
2024-02-06 00:29:27 -0300

    criando tabela no convex e mutation

--- app/(dashboard)/_components/empty-board.tsx
+++ app/(dashboard)/_components/empty-board.tsx
@@ -1,8 +1,29 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
 
 const EmptyBoard = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Meu primeiro board",
+    })
+      .then((id) => {
+        toast.success("Board criado com sucesso");
+      })
+      .catch(() => toast.error("Erro ao criar board"));
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" width={110} height={110} alt="sem resultados" />
@@ -14,7 +35,9 @@ const EmptyBoard = () => {
       </p>
 
       <div className="mt-6">
-        <Button size="lg">Criar um board</Button>
+        <Button disabled={pending} onClick={onClick} size="lg">
+          Criar um board
+        </Button>
       </div>
     </div>
   );
