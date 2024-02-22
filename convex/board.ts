8717ed21a9d301894b006d87876a899544d40db4
Jefferson <jefferson.brandao@g.globo>
2024-02-22 20:38:17 -0300

    criando query de busca e favoritos

--- convex/board.ts
+++ convex/board.ts
@@ -49,6 +49,19 @@ export const remove = mutation({
       throw new Error("Not authenticated")
     }
 
+    const userId = identity.subject
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) => q
+        .eq("userId", userId)
+        .eq("boardId", args.id))
+      .unique()
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id)
+    }
+
     await ctx.db.delete(args.id)
   }
 })
@@ -97,10 +110,9 @@ export const favorite = mutation({
     const userId = identity.subject
 
     const existingsFavorites = await ctx.db.query("userFavorites")
-      .withIndex("by_user_board_org", (q) => q
+      .withIndex("by_user_board", (q) => q
         .eq("userId", userId)
-        .eq("boardId", board._id)
-        .eq("orgId", args.orgId))
+        .eq("boardId", board._id))
       .unique()
 
     if (existingsFavorites) {
