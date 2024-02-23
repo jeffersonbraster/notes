bacad7f89b5670ce7e0de3ecf5cf47c93eec670b
Jefferson <jefferson.brandao@g.globo>
2024-02-22 21:25:06 -0300

    configuração do liveblocks e loading

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
@@ -15,6 +13,12 @@ const Toolbar = () => {
         <div>Redo</div>
       </div>
     </div>
+  );
+};
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md" />
   );
 };
 
